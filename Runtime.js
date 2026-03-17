f79eba6de9bc5fbc5c72015bb08ea332333c5675
TwoSwordsYT <96097757+TwoSwordsYT@users.noreply.github.com>
2026-03-17 00:15:48 -0700

    Runtime.js

--- Runtime.js
+++ Runtime.js
@@ -5,53 +5,58 @@
 <title>Update Log</title>
 
 <style>
-/* Remove all default spacing */
 html, body {
     margin: 0;
     padding: 0;
     background: #000;
 }
 
-/* EXACT SIZE LOCK */
+/* MAIN PANEL */
 .wrapper {
-    width: 254px;
-    height: 353px;
+    width: 241px;
+    height: 335px;
     overflow: hidden;
     border: 1px solid #00bfff;
     font-family: Arial, Helvetica, sans-serif;
     color: #00bfff;
 }
 
-/* HEADER IMAGE */
-.header-img {
-    width: 254px;
-    height: 50px;
+/* HEADER IMAGE FIX */
+.header-wrap {
+    width: 100%;
+    height: 55px;      /* controls banner size */
+    overflow: hidden;
+}
+
+.header-wrap img {
+    width: 100%;
+    height: 100%;
     object-fit: cover;
     display: block;
 }
 
 /* SUBHEADER */
 .subheader {
-    font-size: 9px;
+    font-size: 8px;
     text-align: center;
-    padding: 4px;
+    padding: 3px;
     border-bottom: 1px solid #00bfff;
 }
 
-/* CONTENT AREA */
+/* CONTENT */
 .content {
-    padding: 5px;
-    font-size: 9px;
-    line-height: 1.15;
+    padding: 4px;
+    font-size: 8px;
+    line-height: 1.1;
 }
 
 /* VERSION */
 .version {
     font-weight: bold;
-    margin-top: 4px;
+    margin-top: 3px;
 }
 
-/* BULLET STYLE */
+/* BULLETS */
 .item::before {
     content: "■ ";
 }
@@ -59,7 +64,7 @@ html, body {
 /* DIVIDER */
 .divider {
     border-bottom: 1px solid #00bfff;
-    margin: 5px 0;
+    margin: 4px 0;
 }
 </style>
 </head>
@@ -68,18 +73,39 @@ html, body {
 
 <div class="wrapper">
 
-    <img src="updatelog.png" class="header-img">
+    <!-- HEADER IMAGE -->
+    <div class="header-wrap">
+        <img src="images/updatelog.png">
+    </div>
 
+    <!-- SUBHEADER -->
     <div class="subheader">
         LATEST AVAILABLE BUILD: 0.0.1
     </div>
 
+    <!-- CONTENT -->
     <div class="content">
 
         <div class="version">V0.0.1</div>
-        <div class="item">made update log</div>
+        <div class="item">ADDED SPOON CHARACTER</div>
+        <div class="item">REMOVED SPOON CHARACTER</div>
+        <div class="item">ADDED DLC CONTENT</div>
+        <div class="item">UPDATED MOVEMENT</div>
+        <div class="item">TWEAKED DLC</div>
+        <div class="item">TWEAKED EARN RATES</div>
 
         <div class="divider"></div>
+
+        <div class="version">V0.0.1</div>
+        <div class="item">EXAMPLE TEXT UPDATE</div>
+        <div class="item">NEW OUTFITS</div>
+
+        <div class="divider"></div>
+
+        <div class="version">V0.0.1</div>
+        <div class="item">TOWNMAP REVAMP</div>
+        <div class="item">KYOTO ADDED</div>
+        <div class="item">FIXED RANKED BUG FIX</div>
 
     </div>
 
